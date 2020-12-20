01fe54aada352472fa2de752d13ffb497f106375
Princiya <princiya.marina@gmail.com>
2020-12-20 19:46:33 +0100

    updated twitter social card code

--- plugins/social-cards/index.js
+++ plugins/social-cards/index.js
@@ -13,57 +13,22 @@ module.exports = async ({ markdownNode }) => {
     /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
     ""
   );
-  // const time = counter ? `${Math.ceil(counter/60)}m` : "3m";
-  const titleFont = await jimp.loadFont(`${__dirname}/fonts/Title.fnt`);
-  const detailFont = await jimp.loadFont(`${__dirname}/fonts/Detail.fnt`);
-  // const titleFont = await jimp.loadFont(jimp.FONT_SANS_64_BLACK);
-  // const tagFont = await jimp.loadFont(jimp.FONT_SANS_16_WHITE);
-  const tagFont = await jimp.loadFont(`${__dirname}/fonts/Tag.fnt`);
+  const titleFont = await jimp.loadFont(`${__dirname}/fonts/OpenSans.fnt`);
   
   return Promise.all([jimp.read(`${__dirname}/templates/wave.png`)]).then(
     ([image]) => {
-      // image.print(detailFont, 77, 20, time, 1100).write(location)
-      // image.print(detailFont, 77, 30, tags.join(','), 1100).write(location)
       image
         .print(
           titleFont,
-          77,
+          10,
           10,
           {
             text: cleanedTitle,
             alignmentX: jimp.HORIZONTAL_ALIGN_LEFT,
-            alignmentY: jimp.VERTICAL_ALIGN_TOP,
+            alignmentY: jimp.VERTICAL_ALIGN_MIDDLE,
           },
-          700,
-          300
-        )
-        .write(location)
-      image
-        .print(
-          tagFont,
-          77,
-          20,
-          {
-            text: `Category: ${tags[0]}`,
-            alignmentX: jimp.HORIZONTAL_ALIGN_LEFT,
-            alignmentY: jimp.VERTICAL_ALIGN_BOTTOM,
-          },
-          700,
-          300
-        )
-        .write(location)
-      image
-        .print(
-          detailFont,
           300,
-          20,
-          {
-            text: 'princiya.com',
-            alignmentX: jimp.HORIZONTAL_ALIGN_RIGHT,
-            alignmentY: jimp.VERTICAL_ALIGN_BOTTOM,
-          },
-          700,
-          300
+          250
         )
         .write(location)
     }
